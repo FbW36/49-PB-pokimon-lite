189d5eca68d47f50630fcbaa7aa2baf1b91ba88a
Elias Guderian <46761652+GuderianE@users.noreply.github.com>
2020-11-25 17:57:48 +0100

    please review my code

--- Elias/pokemon-lite.js
+++ Elias/pokemon-lite.js
@@ -1,53 +1,92 @@
 class Pokemon {
-    constructor(pokemon, health, mana, attackSkill) {
+    constructor(pokemon, health, mana, attackSkill = ["none"]) {
         this.pokemon = pokemon;
         this.health = health;
         this.mana = mana;
         this.attackSkill = attackSkill;
     }
-    newPokemon (attackskill) {
+    newPokemon(attackskill) {
         const newPokemon = {
             name: this.pokemon,
             health: this.health,
             mana: this.mana,
-            attackSkills: this.attackSkill = [],
+            attackSkills: [this.attackSkill],
         }
         return newPokemon;
     }
 }
-class Skill extends Pokemon  {
-    constructor () {
-        super();
-        this.learnAttackSkill = (obj) => {
-            this.newPokemon(this.attackSkill.push(obj));
+class Skill extends Pokemon {
+    constructor(health, mana, attackSkill) {
+        super(health, mana, attackSkill);
+        this.learnAttackSkill = (pokemon, obj) => {
+            let newPokemonSkill = pokemon.attackSkill.push(obj);
+            if (pokemon.attackSkill.includes("none")) {
+                pokemon.attackSkill.shift();
+            }
+            return newPokemonSkill;
         }
     }
-    
-    // showStatus: function() {
-
-    // },
-    // attack: function() {
-
-    // }
+    showStatus(pokemon) {
+        let status = `${pokemon.pokemon} \n\n Health: ${pokemon.health}, Mana: ${pokemon.mana}`;
+        return status;
+    }
+    attack(pokemon, skill, enemy) {
+        let damagedealt = 0;
+        let manaUsed = 0;
+        let enemyHealth = 0;
+        if (skill.manacost < pokemon.mana) {
+            enemyHealth = enemy.health - skill.damage;
+            damagedealt = skill.damage;
+            manaUsed = pokemon.mana - skill.manacost;
+            enemy.health = enemyHealth;
+            pokemon.mana = manaUsed;
+            return `${pokemon.pokemon} deals ${damagedealt} damage to ${enemy.pokemon}!`;
+        } else if (pokemon.mana === 0) {
+            return this.refillMana(pokemon);
+        } else if (enemy.health === 0) {
+            return `You defeted ${enemy}!`;
+        } else {
+            return "You're Pokemon does not have enough mana!";
+        }
+    }
+    refillMana(pokemon) {
+        pokemon.mana += 10;
+    }
 }
 
 const AttackSkill = function (skill, damage, manacost) {
-        this.skill = skill;
-        this.damage = damage;
-        this.manacost = manacost;
+    this.skill = skill;
+    this.damage = damage;
+    this.manacost = manacost;
 
-        const newAttackSkill = {
-            skill: this.skill,
-            damage: this.damage,
-            manacost: this.manacost
-        }
-        return newAttackSkill;
+    const newAttackSkill = {
+        skill: this.skill,
+        damage: this.damage,
+        manacost: this.manacost
     }
+    return newAttackSkill;
+}
 
-    let Pikachu = new Pokemon("Pikachu", 100, 50);
-    console.log(Pikachu);
-    let lightning = new AttackSkill("lightning", 40, 30);
-    let skill = new Skill();
-    console.log(lightning);
-    skill.learnAttackSkill(lightning)
-    console.log(Pikachu);
+let Pikachu = new Pokemon("Pikachu", 100, 50);
+let Glumanga = new Pokemon("Glumanga", 200, 40);
+console.log(Pikachu);
+let lightning = new AttackSkill("lightning", 40, 30);
+let tackle = new AttackSkill("tackle", 10, 5);
+let fireBlast = new AttackSkill("fire blast", 60, 40);
+let skill = new Skill();
+skill.learnAttackSkill(Pikachu, lightning)
+skill.learnAttackSkill(Pikachu, tackle)
+skill.learnAttackSkill(Glumanga, fireBlast)
+console.log(skill.showStatus(Pikachu))
+console.log(Pikachu);
+console.log(Glumanga);
+console.log(skill.attack(Pikachu, lightning, Glumanga));
+console.log(skill.showStatus(Pikachu))
+console.log(skill.showStatus(Glumanga))
+console.log(skill.attack(Glumanga, tackle, Pikachu));
+console.log(skill.showStatus(Pikachu))
+skill.refillMana(Pikachu)
+console.log(skill.attack(Pikachu, lightning, Glumanga));
+console.log(skill.attack(Glumanga, fireBlast, Pikachu));
+console.log(skill.showStatus(Pikachu))
+
